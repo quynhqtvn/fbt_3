0bc9810bac24eae4aec7916d572530bb8fc12722
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-10-28 16:52:29 +0700

    RSpec test model comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,37 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_review
-  before_action :load_comment, except: :create
-
-  def index
-    @comments = Comment.latest.paginate page: params[:page],
-      per_page: Settings.per_page
-  end
+  load_and_authorize_resource
 
   def create
-    @comment = @review.comments.create comment_params
-    if @comment.save
-      respond_to do |format|
-        format.html do
-          flash[:success] = t "comment.added"
-          redirect_to @review
-        end
-        format.js
-      end
-    end
-  end
-
-  def edit
+    @comment = current_user.comments.build comment_params
     respond_to do |format|
-      format.js
-    end
-  end
-
-  def update
-    @comment.update_attributes comment_params
-    respond_to do |format|
+      format.html {redirect_to @comment.review}
       format.js
     end
   end
 
   def destroy
-    if @comment.destroy
-      respond_to do |format|
-        format.html do
-          flash[:success] = t "comment.deleted"
-          redirect_to @review
-        end
-        format.js
-      end
+    @comment.destroy
+    respond_to do |format|
+      format.html {redirect_to @comment.review}
+      format.js
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @comment.update_attributes comment_params
+    respond_to do |format|
+      format.html {redirect_to @comment.review}
+      format.js
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge! user_id: current_user.id
+    params.require(:comment).permit :content, :review_id
   end
 
   def load_review
